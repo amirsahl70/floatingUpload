497511a365c842c1c300456124f4095c274676a2
Amir <amir@Amirs-Mac.local>
2018-08-04 23:35:51 -0700

    Initial Commit

---
+++ floatingUpload/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  floatingUpload
+//
+//  Created by Amir on 8/4/2016.
+//  Copyright © 2018 uni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
